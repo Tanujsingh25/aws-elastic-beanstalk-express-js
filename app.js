1980adb991ef90772208b836625585356ade6246
tanuj kumar <tanujsingh332@gmail.com>
2023-04-08 12:30:25 +0530

    Full pipeline test with manual review part-2

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! My First Devops Project'));
-app.get('/', (req, res) => res.send('My first codeBuild Pipeline'));
+app.get('/', (req, res) => res.send('Hello World! My First Devops Project & My first codeBuild Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
